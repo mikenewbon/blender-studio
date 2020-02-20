a6d3b996bc6f43a4644e40d1a985b0d4857dedf3
Sem Mulder <sem@blender.studio>
2020-02-20 11:00:53 +0100

    Fix linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-	env: {
-		browser: true,
-		es6: true,
-	},
-	extends: [
-		'airbnb-base',
-	],
-	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly',
-	},
-	parserOptions: {
-		ecmaVersion: 2018,
-	},
-	rules: {}
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        'no-underscore-dangle': 'off',
+        'no-implicit-globals': ['error', {'lexicalBindings': true}],
+    }
 };
--- training_main/static/training_main/scripts/components/card_training.js
+++ training_main/static/training_main/scripts/components/card_training.js
@@ -1,34 +1,42 @@
-class CardTraining {
-    constructor(element) {
-        this.element = element;
-        this.favoriteUrl = element.dataset.favoriteUrl;
-        this.favoriteElement = this.element.getElementsByClassName('card-training-favorite')[0];
-        this._setupEventListeners();
+'use strict';
+/* global ajax:false */
+
+window.cardTraining = (function cardTraining() {
+    class CardTraining {
+        constructor(element) {
+            this.element = element;
+            this.favoriteUrl = element.dataset.favoriteUrl;
+            [this.favoriteElement] = this.element.getElementsByClassName('card-training-favorite');
+            this._setupEventListeners();
+        }
+
+        _setupEventListeners() {
+            this.favoriteElement.addEventListener('click', this._favoriteClicked.bind(this));
+        }
+
+        _favoriteClicked() {
+            ajax.jsonRequest(
+                'POST',
+                this.favoriteUrl,
+                {
+                    favorite: !this.favoriteElement.dataset.checked,
+                },
+            ).then((data) => {
+                if (data.favorite) {
+                    this.favoriteElement.dataset.checked = 'checked';
+                } else {
+                    delete this.favoriteElement.dataset.checked;
+                }
+            });
+        }
     }
 
-    _setupEventListeners() {
-        this.favoriteElement.addEventListener('click', this._favoriteClicked.bind(this));
-    }
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementsByClassName('card-training').forEach((card) => {
+            // TODO(sem): How to store references to these JS Objects?
+            new CardTraining(card); // eslint-disable-line no-new
+        });
+    });
 
-    _favoriteClicked() {
-        jsonRequest(
-            'POST',
-            this.favoriteUrl,
-            {
-                'favorite': !Boolean(this.favoriteElement.dataset.checked)
-            }
-        ).then((data) => {
-            if (data['favorite']) {
-                this.favoriteElement.dataset.checked = "checked";
-            } else {
-                delete this.favoriteElement.dataset.checked;
-            }
-        })
-    }
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    for (let card of document.getElementsByClassName('card-training')) {
-        new CardTraining(card)
-    }
-});
+    return {};
+}());
