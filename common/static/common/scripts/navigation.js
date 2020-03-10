fa4f6fe75b9cf534d6c4553025744d3f67c8e912
Mike Newbon <michael.newbon@gmail.com>
2020-03-10 16:05:56 +0100

    Added Cookie tracking to navigation open

--- common/static/common/scripts/navigation.js
+++ common/static/common/scripts/navigation.js
@@ -1,20 +1,38 @@
 /* global $:false */
 
 (function navigation() {
-  function navDrawerToggle() {
-    document.querySelector('body').classList.toggle('nav-drawer-open');
-  }
+	function navDrawerToggle() {
+		document.querySelector('body').classList.toggle('nav-drawer-open');
+	}
 
-  document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.navdrawertoggle').forEach(i => {
-      i.addEventListener('click', () => {
-        navDrawerToggle();
-      });
-    });
-  });
+	function setCookie(){
+		if (document.querySelector('body').classList.contains('nav-drawer-open')){
+			Cookies.set('navDrawerOpen', true)
+		} else{
+			Cookies.set('navDrawerOpen', false)
+		}
+	}
 
-  // TODO(sem): Why do we wrap this function in `$`? What does that do?
-  $(() => {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+	function checkNavOpen(){
+		if (Cookies.get('navDrawerOpen') == "true"){
+			document.querySelector('body').classList.add('nav-drawer-open');
+		} else{
+			document.querySelector('body').classList.remove('nav-drawer-open');
+		}
+	}
+
+	document.addEventListener('DOMContentLoaded', () => {
+		checkNavOpen();
+		document.querySelectorAll('.navdrawertoggle').forEach(i => {
+			i.addEventListener('click', () => {
+				navDrawerToggle();
+				setCookie();
+			});
+		});
+	});
+
+	// TODO(sem): Why do we wrap this function in `$`? What does that do?
+	$(() => {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
 })();
