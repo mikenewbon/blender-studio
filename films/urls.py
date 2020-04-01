31c20e5d9088fe777d9862ac98f32ff569adc384
Mike Newbon <mike@blender.studio>
2020-04-01 20:11:17 +0100

    Completed responsive - segmented pages

--- films/urls.py
+++ films/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from films.views.films.film import FilmListView, FilmDetailView
 
 urlpatterns = [
-    path('', FilmListView.as_view(), name='film-list'),
+    # path('', FilmListView.as_view(), name='film-list'),
     path('<slug:slug>', FilmDetailView.as_view(), name='film-detail'),
+    path('', views.index, name='index'),
+    path('coffee-run', views.coffee_run, name='coffee-run'),
+    path('coffee-run/about', views.about, name='about'),
+    path('coffee-run/gallery', views.gallery, name='gallery'),
+    path('coffee-run/weeklies', views.weeklies, name='weeklies'),
 ]
--- films/views.py
+++ films/views.py
@@ -6,3 +6,23 @@ def index(request):
 
     context = {'films': 'filmslist '}
     return render(request, 'films/films_list.html', context)
+
+def coffee_run(request):
+
+    context = {'films': 'about '}
+    return render(request, 'films/coffee-run/coffee-run.html', context)
+
+def about(request):
+
+    context = {'films': 'about '}
+    return render(request, 'films/coffee-run/about.html', context)
+
+def weeklies(request):
+
+    context = {'films': 'weeklies '}
+    return render(request, 'films/coffee-run/weeklies.html', context)
+
+def gallery(request):
+
+    context = {'films': 'about '}
+    return render(request, 'films/coffee-run/gallery.html', context)
