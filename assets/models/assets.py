2bed1fdc256f67a6af884368a62a09501ca7ad04
natkam <natka_m@o2.pl>
2020-06-01 18:32:54 +0200

    Update assets models, fix hashed upload path

--- assets/models/assets.py
+++ assets/models/assets.py
@@ -1,6 +1,7 @@
 import hashlib
 import uuid
 from pathlib import Path
+from typing import Union
 
 from django.contrib.auth.models import User
 from django.db import models
@@ -13,30 +14,28 @@ from films.models.films import Film
 
 
 # TODO: write tests
-def generate_hash_from_filename(instance_uuid: uuid.UUID, filename: str) -> str:
+def generate_hash_from_filename(filename: str) -> str:
     """Combine filename and uuid4 and get a unique string."""
-    unique_filename = f'{instance_uuid}_{filename}'
+    unique_filename = f'{uuid.uuid4()}_{filename}'
     return hashlib.md5(unique_filename.encode('utf-8')).hexdigest()
 
 
-def get_upload_to_hashed_path(asset: 'Asset', filename: str) -> Path:
-    """ Generate a unique, hashed upload path for an asset file.
+def get_upload_to_hashed_path(asset: Union['Video', 'Image', 'File'], filename: str) -> Path:
+    """ Generate a unique, hashed upload path for an asset source file.
 
-    Every asset file gets a hashed filename. Image files are uploaded directly to MEDIA_ROOT, e.g.
-    MEDIA_ROOT/bd2b5b1cd81333ed2d8db03971f91200.png.
-    For each Video and File model instance, a separate folder with a unique name is created, where
-    all the instance-related files (both the asset file and the preview picture) are stored;
-    the folder name is the asset uuid. E.g.:
-    MEDIA_ROOT/b60123d8-a0b5-44fb-ac88-1cd992e30343/886743af0e9bc9ef7636caf489d5352c.mp4
+    Videos and files will be uploaded to nested directories:
+    MEDIA_ROOT/films/<bd>/<bd2b5b1cd81333ed2d8db03971f91200>/.
+    Images - to MEDIA_ROOT/films/<bd>/
     """
+    # TODO: think of a better parameter name? it's not an Asset instance anymore
     extension = Path(filename).suffix
-    hashed_path = Path(generate_hash_from_filename(asset.asset.uuid, filename))
+    hashed = generate_hash_from_filename(filename)
+    path = Path('films', hashed[:2])
 
     if isinstance(asset, (Video, File)):
-        dir_path = Path(str(asset.asset.uuid))
-        path = dir_path.joinpath(hashed_path).with_suffix(extension)
+        path = path.joinpath(hashed, hashed).with_suffix(extension)
     else:
-        path = hashed_path.with_suffix(extension)
+        path = path.joinpath(hashed).with_suffix(extension)
     return path
 
 
@@ -47,12 +46,13 @@ class AssetCategory(models.TextChoices):
 
 
 class Asset(mixins.CreatedUpdatedMixin, models.Model):
-    """ This represents the leaf of the tree of film-related resources. """
+    """ This represents the leaf of the tree of film-related resources.
+
+    An asset can be of one of the three types: image, video, or file.
+    """
 
     class Meta:
         constraints = []  # TODO: only one related file (img, video, file)
-
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='assets')
     collection = models.ForeignKey(
@@ -72,15 +72,20 @@ class Asset(mixins.CreatedUpdatedMixin, models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='uploaded_assets')
     user.description = "The user who uploaded the asset."
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='authored_assets')
-    author.description = "The actual author of the asset."
+    author.description = "The actual author of the artwork/learning materials."
     view_count = models.PositiveIntegerField(default=0)
 
     visibility = models.BooleanField(default=False)
 
     @property
     def picture_16_9(self):
-        # TODO: get appropriate picture from the related img/video/file
-        pass
+        # TODO: This sends a db query for each attribute check. Use `select_related` in view?
+        if hasattr(self, 'image'):
+            return self.image.picture_16_9
+        if hasattr(self, 'video'):
+            return self.video.picture_16_9
+        if hasattr(self, 'file'):
+            return self.file.picture_16_9
 
     def clean(self) -> None:
         super().clean()
@@ -93,39 +98,45 @@ class Asset(mixins.CreatedUpdatedMixin, models.Model):
 
 class Video(mixins.CreatedUpdatedMixin, models.Model):
     asset = models.OneToOneField(Asset, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_upload_to_hashed_path)
+    source = models.FileField(upload_to=get_upload_to_hashed_path)
+    original_filename = models.CharField(max_length=128)
     size_bytes = models.IntegerField()
     resolution = models.CharField(max_length=32, blank=True)
     resolution_text = models.CharField(max_length=32, blank=True)
     duration_seconds = models.DurationField(help_text='[DD] [[HH:]MM:]ss[.uuuuuu]')
-    preview = models.ImageField(upload_to=get_upload_to_hashed_path)
+    picture_16_9 = models.ImageField(upload_to=get_upload_to_hashed_path, blank=True)
+    # TODO: generate picture_16_9 if not uploaded
 
     play_count = models.PositiveIntegerField(default=0)
 
     def __str__(self) -> str:
-        return f"{self._meta.model_name} {self.file.path} in {self.asset.name}"
+        return f"{self._meta.model_name} {self.source.path} in {self.asset.name}"
 
 
 class Image(mixins.CreatedUpdatedMixin, models.Model):
     asset = models.OneToOneField(Asset, on_delete=models.CASCADE)
-    file = models.ImageField(upload_to=get_upload_to_hashed_path)
+    source = models.ImageField(upload_to=get_upload_to_hashed_path)
+    original_filename = models.CharField(max_length=128)
     size_bytes = models.IntegerField()
     resolution = models.CharField(max_length=32, blank=True)
     resolution_text = models.CharField(max_length=32, blank=True)
+    picture_16_9 = models.ImageField(upload_to=get_upload_to_hashed_path, blank=True)
 
     def __str__(self) -> str:
-        return f"{self._meta.model_name} {self.file.path} in {self.asset.name}"
+        return f"{self._meta.model_name} {self.source.path} in {self.asset.name}"
 
 
 class File(mixins.CreatedUpdatedMixin, models.Model):
     asset = models.OneToOneField(Asset, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_upload_to_hashed_path)
+    source = models.FileField(upload_to=get_upload_to_hashed_path)
+    original_filename = models.CharField(max_length=128)
     size_bytes = models.IntegerField()
-    preview = models.ImageField(upload_to=get_upload_to_hashed_path)
+    picture_16_9 = models.ImageField(upload_to=get_upload_to_hashed_path, blank=True)
+    # TODO: generate picture_16_9 if not uploaded
 
     def __str__(self) -> str:
-        return f"{self._meta.model_name} {self.file.path} in {self.asset.name}"
+        return f"{self._meta.model_name} {self.source.path} in {self.asset.name}"
 
 
 # TODO: Handle deleting all these files when a model instance is deleted from the db?
-# TODO: size could be retrieved: file.size (cached property of django.core.files.base.File)
+# TODO: size could be retrieved: source.size (cached property of django.core.files.base.File)
