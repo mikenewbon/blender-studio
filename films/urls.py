ac3123c70671734750995328fd2757b9542d622e
natkam <natka_m@o2.pl>
2020-06-05 10:59:06 +0200

    [WIP] Temporarily extract secondary navbar to a component

--- films/urls.py
+++ films/urls.py
@@ -6,9 +6,6 @@ from films.views.films.film import FilmListView, FilmDetailView
 urlpatterns = [
     path('', FilmListView.as_view(), name='film-list'),
     path('<slug:slug>', FilmDetailView.as_view(), name='film-detail'),
-    # path('', views.index, name='index'),
-    # path('spring', views.spring, name='spring'),
-    # path('coffee-run', views.coffee_run, name='coffee-run'),
     path('<slug:slug>/about', views.about, name='film-about'),
     path('<slug:slug>/gallery', views.gallery, name='film-gallery'),
     path('<slug:slug>/weeklies', views.weeklies, name='film-weeklies'),
