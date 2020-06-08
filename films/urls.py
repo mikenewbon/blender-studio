801215d04a96db013cee3d2bb3c59e42bc4fecfb
natkam <natka_m@o2.pl>
2020-06-08 17:58:40 +0200

    Move film-related views to a single module

--- films/urls.py
+++ films/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from films.views import views
-from films.views.films.film import FilmListView, FilmDetailView
+from films.views import films
+from films.views.films import FilmListView, FilmDetailView
 
 urlpatterns = [
     path('', FilmListView.as_view(), name='film-list'),
     path('<slug:slug>', FilmDetailView.as_view(), name='film-detail'),
-    path('<slug:slug>/about', views.about, name='film-about'),
-    path('<slug:slug>/gallery', views.gallery, name='film-gallery'),
-    path('<slug:slug>/weeklies', views.weeklies, name='film-weeklies'),
+    path('<slug:slug>/about', films.about, name='film-about'),
+    path('<slug:slug>/gallery', films.gallery, name='film-gallery'),
+    path('<slug:slug>/weeklies', films.weeklies, name='film-weeklies'),
 ]
--- films/views/films.py
+++ films/views/films.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_safe
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from films.models import Film
+
+
+@method_decorator(require_safe, name='dispatch')
+class FilmListView(ListView):
+    model = Film
+    queryset = Film.objects.filter(is_published=True).order_by('status', '-release_date')
+    template_name = 'films/films.html'
+
+
+@method_decorator(require_safe, name='dispatch')
+class FilmDetailView(DetailView):
+    queryset = Film.objects.filter(is_published=True)
 
 
 def about(request, slug):
