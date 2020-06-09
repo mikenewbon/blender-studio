69a2281bdcb2b2d62b2543b1e7a3baa246a14411
natkam <natka_m@o2.pl>
2020-06-09 14:33:11 +0200

    Order nested collections properly

--- films/views/gallery.py
+++ films/views/gallery.py
@@ -1,32 +1,32 @@
-from typing import List, Dict
+from typing import Dict
 
 from django.db.models import QuerySet
+from django.db.models.query import Prefetch
 from django.shortcuts import get_object_or_404, render
 
-from films.models import Film, Collection, Asset
+from films.models import Film, Collection
 
 
 def film_collection_list(request, slug):
     film = get_object_or_404(Film, slug=slug)
-    top_collections = (
+    top_level_collections = (
         film.collections.filter(parent__isnull=True)
         .order_by('order')
-        .prefetch_related('child_collections')
+        .prefetch_related(
+            Prefetch(
+                'child_collections', queryset=film.collections.order_by('order'), to_attr='nested'
+            )
+        )
     )
 
     nested_collections: Dict[Collection, QuerySet[Collection]] = dict()
-    for c in top_collections:
-        nested_collections[c] = c.child_collections.all()
-
-    static_assets = film.storage.assets.prefetch_related('assets')
-    featured_artwork: List[Asset] = []
-    for asset in static_assets:
-        featured_artwork.extend(asset.assets.filter(is_featured=True))
+    for c in top_level_collections:
+        nested_collections[c] = c.nested
 
     context = {
         'film': film,
         'collections': nested_collections,
-        'featured_artwork': film.assets.filter(is_featured=True),
+        'featured_artwork': film.assets.filter(is_featured=True, is_published=True),
     }
 
     return render(request, 'films/gallery.html', context)
