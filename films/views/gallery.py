d76197badadd2bea24c6d878ed0fe0a9523e3049
natkam <natka_m@o2.pl>
2020-06-09 10:31:40 +0200

    Retrieve nested collections

--- films/views/gallery.py
+++ films/views/gallery.py
@@ -5,5 +5,16 @@ from films.models import Film
 
 def film_collection_list(request, slug):
     film = get_object_or_404(Film, slug=slug)
-    context = {'film': film, 'collections': film.collections.filter(parent__isnull=True)}
+    top_collections = (
+        film.collections.filter(parent__isnull=True)
+        .order_by('order')
+        .prefetch_related('child_collections')
+    )
+
+    nested_collections = dict()
+    for c in top_collections:
+        nested_collections[c] = c.child_collections.all()
+
+    context = {'film': film, 'collections': nested_collections}
+
     return render(request, 'films/gallery.html', context)
