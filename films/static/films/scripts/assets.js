4d452d4be684eb7be8ba5b38bca2524d100b3e58
natkam <natka_m@o2.pl>
2020-06-12 19:35:56 +0200

    Create a single modal for displaying assets, remove unused code blocks

--- films/static/films/scripts/assets.js
+++ films/static/films/scripts/assets.js
@@ -4,24 +4,25 @@
 window.asset = (function asset() {
     document.addEventListener('DOMContentLoaded', () => {
         document.getElementsByClassName('file-body').forEach(element => {
-
             element.addEventListener('click', event => {
+                fetch(element.dataset.url).then(response => {
+                    if (response.status >= 200 && response.status < 300) {
+                        return Promise.resolve(response);
+                    }
+                    return Promise.reject(new Error(response.statusText));
+                }).then(response => {
+                    const html = response.text()
+                    const template = document.createElement('template');
+                    template.innerHTML = html.trim();
 
-                fetch(element.dataset.url).then(function (response) {
-                    // The API call was successful!
-                    return response.text();
-                }).then(function (html) {
-                    // This is the HTML from our response as a text string
-                    document.getElementById("#gallery-modal").appendChild(html);
-                }).catch(function (err) {
-                    // There was an error
-                    console.warn('Something went wrong.', err);
-                });
-
-
+                    const modal = document.getElementById("gallery-modal")
+                    if (modal.childElementCount !== 0) {
+                        modal.children[0].replaceWith(template.content);
+                    } else {
+                        modal.appendChild(template.content);
+                    }
+                })
             })
         });
     });
-
-    return 42
 })();
--- films/views/api/asset.py
+++ films/views/api/asset.py
@@ -1,11 +1,11 @@
-from django.http import JsonResponse
+from django.http import HttpResponse
 from django.http.request import HttpRequest
 from django.shortcuts import render
 
 from films.models import Asset
 
 
-def asset(request: HttpRequest, film_pk: int, asset_pk: int):
+def asset(request: HttpRequest, film_pk: int, asset_pk: int) -> HttpResponse:
     asset = (
         Asset.objects.filter(pk=asset_pk)
         .select_related(
@@ -19,18 +19,3 @@ def asset(request: HttpRequest, film_pk: int, asset_pk: int):
     )
 
     return render(request, 'components/modal_asset.html', {'asset': asset})
-
-    return JsonResponse(
-        {
-            'id': asset.pk,
-            'name': asset.name,
-            'type': asset.static_asset.source_type,
-            'collection': asset.collection.name if asset.collection else None,
-            'date_created': asset.date_created,
-            'license': asset.static_asset.license.name,
-            'author': asset.static_asset.author_name,
-            'user': asset.static_asset.user.get_full_name(),
-            'description': asset.description,
-            'source_url': asset.static_asset.source.url,
-        }
-    )
