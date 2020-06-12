5b8083f8c24c00b88f348e12490ee4ca9a1d5821
Francesco Siddi <francesco.siddi@gmail.com>
2020-06-12 11:43:18 +0200

    Assets: Add support for storage backend categories

--- assets/models/storages.py
+++ assets/models/storages.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
+class StorageBackendCategoryChoices(models.TextChoices):
+    local = 'local', 'Local Storage'
+    gcs = 'gcs', 'Google Cloud Storage'
+
+
 class StorageBackend(models.Model):
     name = models.CharField(max_length=512)
+    category = models.CharField(choices=StorageBackendCategoryChoices.choices, max_length=5,
+                                default=StorageBackendCategoryChoices.local)
+    bucket_name = models.CharField(max_length=512, null=True, blank=True)
 
     def __str__(self):
         return self.name
