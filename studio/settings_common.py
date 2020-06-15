3ca3ded9287d44d9980398234c72a52ea94554c0
Mike Newbon <michael.newbon@gmail.com>
2020-06-15 16:02:44 +0200

    Fixed training dropdown

--- studio/settings_common.py
+++ studio/settings_common.py
@@ -57,7 +57,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'training.context_processors.enums',
             ],
             'undefined': jinja2.StrictUndefined,
             'environment': 'common.jinja2.environment',
@@ -78,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'training.context_processors.enums',
             ]
         },
     },
