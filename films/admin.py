5ddbbcc3973e4c120ac39e273180b935c21fe0d3
natkam <natka_m@o2.pl>
2020-06-18 11:09:12 +0200

    Customise admin panel for production log models

--- films/admin.py
+++ films/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from films.models import collections, films, assets
+from films.models import collections, films, assets, production_logs
+
+
+class ProductionLogEntryAssetInline(admin.TabularInline):
+    show_change_link = True
+    model = production_logs.ProductionLogEntryAsset
+
+
+@admin.register(production_logs.ProductionLogEntry)
+class ProductionLogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'production_log__start_date'
+    list_display = ['__str__', 'production_log']
+    inlines = [ProductionLogEntryAssetInline]
+
+
+@admin.register(production_logs.ProductionLog)
+class ProductionLogAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'name']
+
 
 admin.site.register(assets.Asset)
 admin.site.register(collections.Collection)
