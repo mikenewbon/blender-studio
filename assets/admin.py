d42e4290fe63b554cdc9e7534f3028f4ab5815d0
natkam <natka_m@o2.pl>
2020-06-19 08:38:47 +0200

    [WIP] Admin: Static Asset with inlines for Image and Video

--- assets/admin.py
+++ assets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from assets.models import assets, licenses, storages
+from assets.models import assets, licenses, storages, AssetFileTypeChoices
 
 
 @admin.register(licenses.License)
@@ -8,7 +8,43 @@ class LicenseAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(assets.StaticAsset)
-admin.site.register(assets.Image)
-admin.site.register(assets.Video)
+class ImageInline(admin.TabularInline):
+    model = assets.Image
+    extra = 0
+
+
+class VideoInline(admin.TabularInline):
+    model = assets.Video
+    extra = 0
+
+
+@admin.register(assets.StaticAsset)
+class StaticAssetAdmin(admin.ModelAdmin):
+    inlines = [ImageInline, VideoInline]
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': [
+                    field.name
+                    for field in assets.StaticAsset._meta.get_fields()
+                    if field.editable and field.name != 'id'
+                ],
+            },
+        ),
+        (
+            'If you are uploading an image or a video',
+            {
+                'fields': (),
+                'description': 'The fields below depend on the source type of the uploaded '
+                'asset. Add an <strong>Image</strong> if you are uploading an '
+                'image, or a <strong>Video</strong> for video uploads.',
+            },
+        ),
+    )
+    list_filter = ['source_type', 'user', 'storage_backend__film__title']
+
+
+# admin.site.register(assets.Image)
+# admin.site.register(assets.Video)
 admin.site.register(storages.StorageBackend)
