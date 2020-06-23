6b60262a3551902d7f1aae842fa4935479017eb9
natkam <natka_m@o2.pl>
2020-06-23 14:23:23 +0200

    Admin: filter assets by creation date in production log entry form

--- films/admin/production_logs.py
+++ films/admin/production_logs.py
@@ -1,12 +1,14 @@
+import datetime as dt
 from typing import Optional, Any
 
 from django.contrib import admin
 from django.db.models import ForeignKey, Q
 from django.forms.models import ModelChoiceField
 from django.http.request import HttpRequest
+from django.utils import timezone
 
 from films.admin.mixins import EditLinkMixin
-from films.models import production_logs, Asset
+from films.models import production_logs, Asset, ProductionLog
 
 
 class ProductionLogEntryAssetInline(admin.StackedInline):
@@ -18,10 +20,13 @@ class ProductionLogEntryAssetInline(admin.StackedInline):
         self, db_field: 'ForeignKey[Any, Any]', request: Optional[HttpRequest], **kwargs: Any
     ) -> Optional[ModelChoiceField]:
         if db_field.name == 'asset':
+            # Only show published assets created in the last 7 days by the current user
+            # TODO(Natalia): add filtering by film, show assets since the last log
             kwargs['queryset'] = Asset.objects.filter(
                 Q(static_asset__author=request.user)
                 | (Q(static_asset__author__isnull=True) & Q(static_asset__user=request.user)),
                 is_published=True,
+                date_created__gte=timezone.now() - dt.timedelta(days=7),
             )
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
