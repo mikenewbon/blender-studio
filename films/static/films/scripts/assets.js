e99d68dfeabc692220184d30f5873f366c9aa765
Mike Newbon <michael.newbon@gmail.com>
2020-06-23 15:19:44 +0200

    seperated files.html and cleaned up assets/weeklie

--- films/static/films/scripts/assets.js
+++ films/static/films/scripts/assets.js
@@ -2,25 +2,32 @@
 // TODO(Natalia): Figure out what to assign it to (window.asset? and why?)
 
 window.asset = (function asset() {
-    document.addEventListener('DOMContentLoaded', () => {
-        document.getElementsByClassName('file-body').forEach(element => {
-            element.addEventListener('click', event => {
-                fetch(element.dataset.url).then(response => {
-                    return response.text();
-                }).then(html => {
-                    const template = document.createElement('template');
-                    template.innerHTML = html.trim();
+	document.addEventListener('DOMContentLoaded', () => {
+		document.querySelectorAll('.file a[data-toggle*="modal"]').forEach(element => {
+			element.addEventListener('click', event => {
+				fetch(element.dataset.url).then(response => {
+					return response.text();
+				}).then(html => {
+					const template = document.createElement('template');
+					template.innerHTML = html.trim();
 
-                    const modal = document.getElementById('gallery-modal')
-                    if (modal.childElementCount === 0) {
-                        modal.appendChild(template.content);
-                    } else {
-                        modal.children[0].replaceWith(template.content);
-                    }
-                }).catch(err => {
-                    console.warn('Something went wrong.', err);
-                });
-            });
-        });
-    });
+					const modal = document.getElementById('gallery-modal')
+					if (modal.childElementCount === 0) {
+						modal.appendChild(template.content);
+					} else {
+						modal.children[0].replaceWith(template.content);
+					}
+					$(modal).on('hidden.bs.modal', event => {
+						modal.innerHTML = "";
+					})
+					//TODO(Mike): When Bootstrap 5 is added, switch to regular JS.
+					// modal.addEventListener('hidden.bs.modal', event =>{
+					// 	modal.innerHTML="";
+					// })
+				}).catch(err => {
+					console.warn('Something went wrong.', err);
+				});
+			});
+		});
+	});
 })();
