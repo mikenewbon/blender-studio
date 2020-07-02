d400d2491735ea9180c35e51bc2fc1a9a44815b9
Natalia Maniakowska <natalia@blender.studio>
2020-07-02 13:28:07 +0200

    Fix pagination in weeklies view

--- films/views/api/production_logs.py
+++ films/views/api/production_logs.py
@@ -4,14 +4,15 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_safe
 
 from films.models import Film
-from films.views.weeklies import get_production_logs_for_context, DEFAULT_LOGS_PAGE_SIZE
+from films.views.weeklies import get_production_logs_for_context
 
 
 @require_safe
 def production_logs_page(request: HttpRequest, film_pk: int) -> HttpResponse:
     film = get_object_or_404(Film, id=film_pk, is_published=True)
-    page_size = request.GET.get('page_size', DEFAULT_LOGS_PAGE_SIZE)
-    production_logs_page = get_production_logs_for_context(request, film, page_size)
+    page_number = request.GET.get('page')
+    per_page = request.GET.get('per_page')
+    production_logs_page = get_production_logs_for_context(film, page_number, per_page)
     context = {'production_logs_page': production_logs_page}
 
     return render(request, 'films/components/activity_feed.html', context)
--- films/views/weeklies.py
+++ films/views/weeklies.py
@@ -1,4 +1,6 @@
-from django.core.paginator import Page, Paginator
+from typing import Optional, Union
+
+from django.core.paginator import Paginator
 from django.db.models.query import Prefetch, QuerySet
 from django.http import HttpResponse
 from django.http.request import HttpRequest
@@ -9,15 +11,31 @@ from films.models import Film, ProductionLog, ProductionLogEntryAsset
 DEFAULT_LOGS_PAGE_SIZE = 3
 
 
-def get_production_logs_for_context(film: Film) -> 'QuerySet[ProductionLog]':
+def get_production_logs_for_context(
+    film: Film,
+    page_number: Optional[Union[int, str]] = 1,
+    per_page: Optional[Union[int, str]] = DEFAULT_LOGS_PAGE_SIZE,
+) -> 'QuerySet[ProductionLog]':
     """Retrieve production logs for film weeklies context.
 
-    The returned queryset contains production logs and all their related objects
-    used in templates:
-     - production log entries (under the `entries` attribute of a production log),
-     - assets and static assets related to log entries,
-     - entries authors and users (used to get each entry's author_name).
     Altogether, this function sends 5 database queries.
+
+    Args:
+        film: A Film model instance
+        page_number: (optional) int; production logs page number, used by the paginator.
+            By default, the first page.
+        per_page: (optional) int or str; the number of logs to display per page, used
+            by the paginator. Defaults to DEFAULT_LOGS_PAGE_SIZE.
+
+    Returns:
+        A queryset containing production logs and all their related objects used in templates:
+         - production log entries,
+         - entries' authors and users (used to get each entry's author_name),
+         - assets and static assets related to log entries. Note that entries' related
+            `entry_assets` are available under the `assets` attribute (set in Prefetch).
+            These objects are stored in a Python list, which is supposed to improve
+            performance (see the note in the docs:
+            https://docs.djangoproject.com/en/dev/ref/models/querysets/#django.db.models.Prefetch).
     """
     production_logs = film.production_logs.order_by('-start_date').prefetch_related(
         'log_entries__author',
@@ -30,25 +48,19 @@ def get_production_logs_for_context(film: Film) -> 'QuerySet[ProductionLog]':
             to_attr='assets',
         ),
     )
-    production_log_dict = {}
-    for log in production_logs:
-        production_log_dict[log] = {}
-        for entry in log.log_entries.all():
-            production_log_dict[log][entry] = [entry_asset.asset for entry_asset in entry.assets]
+    paginator = Paginator(production_logs, per_page)
+    production_logs_page = paginator.get_page(page_number)
 
-    # TODO(Natalia): paginator accepts a query set, not a dict
-    # page_number = request.GET.get('page')
-    # paginator = Paginator(production_logs, page_size)
-    # production_logs_page = paginator.get_page(page_number)
-
-    return production_log_dict
+    return production_logs_page
 
 
 def production_log_list(request: HttpRequest, film_slug: str) -> HttpResponse:
     film = get_object_or_404(Film, slug=film_slug, is_published=True)
+    page_number = request.GET.get('page')
+    per_page = request.GET.get('per_page')
     context = {
         'film': film,
-        'production_logs': get_production_logs_for_context(film),
+        'production_logs': get_production_logs_for_context(film, page_number, per_page),
     }
 
     return render(request, 'films/weeklies.html', context)
