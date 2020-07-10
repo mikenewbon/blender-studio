0e594f15d71b66349d774229bd426614e002ada4
Mike Newbon <michael.newbon@gmail.com>
2020-07-10 15:20:51 +0200

    Fix various responsive issues

--- common/static/common/scripts/navigation.js
+++ common/static/common/scripts/navigation.js
@@ -1,55 +1,82 @@
 /* global $:false */
 
 (function navigation() {
-	function navDrawerToggle() {
-		document.querySelector('body').classList.toggle('nav-drawer-open');
-	}
+  function navDrawerToggle() {
+    document.querySelector('body').classList.toggle('nav-drawer-open');
+  }
 
-	function setCookie(){
-		if (document.querySelector('body').classList.contains('nav-drawer-open')){
-			Cookies.set('navDrawerOpen', true)
-		} else{
-			Cookies.set('navDrawerOpen', false)
-		}
-	}
+  function setCookie() {
+    if (document.querySelector('body').classList.contains('nav-drawer-open')) {
+      Cookies.set('navDrawerOpen', true)
+    } else {
+      Cookies.set('navDrawerOpen', false)
+    }
+  }
 
-	function checkNavOpen(){
-		if (Cookies.get('navDrawerOpen') == "true"){
-			document.querySelector('body').classList.add('nav-drawer-open');
-		} else{
-			document.querySelector('body').classList.remove('nav-drawer-open');
-		}
-	}
+  function checkNavOpen() {
+    if (Cookies.get('navDrawerOpen') == "true") {
+      document.querySelector('body').classList.add('nav-drawer-open');
+    } else {
+      document.querySelector('body').classList.remove('nav-drawer-open');
+    }
+  }
 
-	document.addEventListener('DOMContentLoaded', () => {
-		checkNavOpen();
-		document.querySelectorAll('.navdrawertoggle').forEach(i => {
-			i.addEventListener('click', () => {
-				navDrawerToggle();
-				setCookie();
-			});
-		});
-	});
+  document.addEventListener('DOMContentLoaded', () => {
+    checkNavOpen();
+    document.querySelectorAll('.navdrawertoggle').forEach(i => {
+      i.addEventListener('click', () => {
+        navDrawerToggle();
+        setCookie();
+      });
+    });
+  });
 
-	// TODO(sem): Why do we wrap this function in `$`? What does that do?
-	$(() => {
-		$('[data-toggle="tooltip"]').tooltip();
-	});
+  // TODO(sem): Why do we wrap this function in `$`? What does that do?
+  $(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 })();
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	// adds active class to any nav item with a href that matches the current url
-	document.querySelectorAll('a.list-group-item[href="' + location.pathname + '"], a.drawer-nav-section-link[href="' + location.pathname + '"], a.drawer-nav-dropdown[href="' + location.pathname + '"]').forEach((link) => {
-		link.classList.add('active');
-	})
+  // adds active class to any nav item with a href that matches the current url
+  document.querySelectorAll('a.list-group-item[href="' + location.pathname + '"], a.drawer-nav-section-link[href="' + location.pathname + '"], a.drawer-nav-dropdown[href="' + location.pathname + '"]').forEach((link) => {
+    link.classList.add('active');
+  })
 
-	// adds active class to main-nav item if it matches the start of the current url
-	document.querySelectorAll('.navbar-main-nav a.list-group-item').forEach((link) => {
-		if(location.pathname.startsWith(link.pathname)) {
-			link.classList.add('active');
-		}
-	});
+  // adds active class to main-nav item if it matches the start of the current url
+  document.querySelectorAll('.navbar-main-nav a.list-group-item').forEach((link) => {
+    if (location.pathname.startsWith(link.pathname)) {
+      link.classList.add('active');
+    }
+  });
 });
 
+// Allow side-scroll drag on desktop
+const slider = document.querySelector(".side-scroll");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", e => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = x - startX;
+  slider.scrollLeft = scrollLeft - walk;
+});
 
