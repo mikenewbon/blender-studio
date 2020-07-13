d873799add3df99c6c110cf9715c59483ed6b6f6
Mike Newbon <michael.newbon@gmail.com>
2020-07-13 12:10:45 +0200

    Fix various responsive issues

--- common/static/common/scripts/navigation.js
+++ common/static/common/scripts/navigation.js
@@ -5,28 +5,11 @@
     document.querySelector('body').classList.toggle('nav-drawer-open');
   }
 
-  function setCookie() {
-    if (document.querySelector('body').classList.contains('nav-drawer-open')) {
-      Cookies.set('navDrawerOpen', true)
-    } else {
-      Cookies.set('navDrawerOpen', false)
-    }
-  }
-
-  function checkNavOpen() {
-    if (Cookies.get('navDrawerOpen') == "true") {
-      document.querySelector('body').classList.add('nav-drawer-open');
-    } else {
-      document.querySelector('body').classList.remove('nav-drawer-open');
-    }
-  }
-
   document.addEventListener('DOMContentLoaded', () => {
-    checkNavOpen();
+
     document.querySelectorAll('.navdrawertoggle').forEach(i => {
       i.addEventListener('click', () => {
         navDrawerToggle();
-        setCookie();
       });
     });
   });
