0038f1df6f7be440c757e408440c7f8ecbcf77d8
Natalia Maniakowska <natalia@blender.studio>
2020-08-04 18:43:30 +0200

    Create search endpoint

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,8 @@
 from django.urls.conf import path
 
-from search.views import search
+from search.views import search, api_search
 
 urlpatterns = [
-    path('api/search', search, name='search'),
+    path('api/search', api_search, name='api-search'),
+    path('', search, name='search'),
 ]
--- search/views.py
+++ search/views.py
@@ -2,12 +2,13 @@ import ast
 
 import meilisearch
 from django.http.request import HttpRequest
-from django.http.response import JsonResponse
-from django.views.decorators.http import require_POST
+from django.http.response import JsonResponse, HttpResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_POST, require_safe
 
 
 @require_POST
-def search(request: HttpRequest):
+def api_search(request: HttpRequest) -> JsonResponse:
     client = meilisearch.Client('http://127.0.0.1:7700')
     index = client.get_index('studio')
     query = request.GET.get('q', '')
@@ -20,3 +21,9 @@ def search(request: HttpRequest):
     results = index.search(query=query, opt_params=opt_params)
 
     return JsonResponse({'results': results})
+
+
+@require_safe
+def search(request: HttpRequest) -> HttpResponse:
+
+    return render(request, 'search/search.html', {})
