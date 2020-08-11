01563930eb0bd4ecfea23c4225dffc638f16355c
Natalia Maniakowska <natalia@blender.studio>
2020-08-11 02:36:29 +0200

    [WIP] Refactor search signals - queryset annotations

---
+++ search/queries.py
@@ -0,0 +1,95 @@
+from typing import Any, Type, Union, Callable
+
+from django.db.models.expressions import F, Value, Case, When
+from django.db.models.fields import CharField
+from django.db.models.functions.text import Concat
+from django.db.models.query import QuerySet
+
+from blog.models import Revision
+from films.models import Film, Asset
+from training.models import Training, Section, TrainingStatus
+
+SearchableModels = Union[Film, Asset, Training, Section, Revision]
+
+
+def get_searchable_queryset(
+    model: Type[SearchableModels], **filter_params: Any
+) -> 'QuerySet[SearchableModels]':
+    function: Callable[..., 'QuerySet[SearchableModels]']
+    if model == Film:
+        function = get_searchable_films
+    elif model == Asset:
+        function = get_searchable_assets
+    elif model == Training:
+        function = get_searchable_trainings
+    elif model == Section:
+        function = get_searchable_sections
+    elif model == Revision:
+        function = get_searchable_posts
+    else:
+        raise TypeError(
+            f'Inappropriate model class. `model` has to be one of Film, Asset, Training, '
+            f'Section, Revision; got {type(model)} instead.'
+        )
+
+    return function(**filter_params).annotate(
+        model=Value('film', output_field=CharField()),
+        search_id=Concat('model', Value('_'), 'id', output_field=CharField()),
+    )
+
+
+def get_searchable_films(**filter_params: Any) -> 'QuerySet[Film]':
+    """Filters and annotates films queryset for the search index.
+
+    Args:
+        **filter_params: any additional filter parameters to filter the queryset by.
+
+    Returns:
+        A filtered and annotated Film queryset.
+    """
+    return Film.objects.filter(is_published=True, **filter_params).annotate(
+        project=F('title'), name=F('title'),
+    )
+
+
+def get_searchable_assets(**filter_params: Any) -> 'QuerySet[Asset]':
+    return Asset.objects.filter(
+        is_published=True, film__is_published=True, **filter_params
+    ).annotate(
+        project=F('film__title'),
+        collection_name=F('collection__name'),
+        license=F('static_asset__license__name'),
+        media_type=F('static_asset__source_type'),
+    )
+
+
+def get_searchable_trainings(**filter_params: Any) -> 'QuerySet[Training]':
+    return Training.objects.filter(status=TrainingStatus.published, **filter_params).annotate(
+        project=F('name'),
+    )
+
+
+def get_searchable_sections(**filter_params: Any) -> 'QuerySet[Section]':
+    return Section.objects.filter(
+        chapter__training__status=TrainingStatus.published, **filter_params
+    ).annotate(
+        project=F('chapter__training__name'),
+        chapter_name=F('chapter__name'),
+        description=F('text'),
+    )
+
+
+def get_searchable_posts(**filter_params: Any) -> 'QuerySet[Revision]':
+    return (
+        Revision.objects.filter(is_published=True, post__is_published=True, **filter_params)
+        .order_by('post_id', '-date_created')
+        .distinct('post_id')
+        .annotate(
+            project=Case(
+                When(post__film__isnull=False, then=F('post__film__title')),
+                default=Value(''),
+                output_field=CharField(),
+            ),
+            name=F('title'),
+        )
+    )
--- search/signals.py
+++ search/signals.py
@@ -1,23 +1,23 @@
 import json
 import logging
-from typing import Union, Type, Any, Dict
+from typing import Type, Any
 
 import meilisearch
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models.expressions import F, Value, Case, When
-from django.db.models.fields import CharField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from blog.models import Revision
 from films.models import Film, Asset
 from search.management.commands.create_search_index import SEARCHABLE_ATTRIBUTES
-from training.models import Training, Section, TrainingStatus
+from search.queries import (
+    SearchableModels,
+    get_searchable_queryset,
+)
+from training.models import Training, Section
 
 log = logging.getLogger(__name__)
-
-SearchableModels = Union[Film, Asset, Training, Section, Revision]
 
 
 @receiver(post_save, sender=Film)
@@ -28,59 +28,25 @@ SearchableModels = Union[Film, Asset, Training, Section, Revision]
 def update_search_index(
     sender: Type[SearchableModels], instance: SearchableModels, **kwargs: Any
 ) -> None:
-    instance_dict: Dict[str, Any] = {}
-    if isinstance(instance, Film) and instance.is_published:
-        instance_qs = sender.objects.filter(id=instance.id).annotate(
-            project=F('title'), name=F('title'),
-        )
+    instance_qs = get_searchable_queryset(sender)
+    if instance_qs:
         instance_dict = instance_qs.values().get()
-        instance_dict['thumbnail_url'] = (
-            instance.picture_16_9.url if instance.picture_16_9 else instance.picture_header.url
-        )
-    elif isinstance(instance, Asset) and instance.is_published and instance.film.is_published:
-        instance_qs = sender.objects.filter(id=instance.id).annotate(
-            project=F('film__title'),
-            collection_name=F('collection__name'),
-            license=F('static_asset__license__name'),
-            media_type=F('static_asset__source_type'),
-        )
-        instance_dict = instance_qs.values().get()
-        instance_dict['thumbnail_url'] = (
-            instance.static_asset.preview.url if instance.static_asset.preview else ''
-        )
-    elif isinstance(instance, Training) and instance.status == TrainingStatus.published:
-        instance_qs = sender.objects.filter(id=instance.id).annotate(project=F('name'),)
-        instance_dict = instance_qs.values().get()
-        instance_dict['thumbnail_url'] = instance.picture_16_9.url
-    elif (
-        isinstance(instance, Section)
-        and instance.chapter.training.status == TrainingStatus.published
-    ):
-        instance_qs = sender.objects.filter(id=instance.id).annotate(
-            project=F('chapter__training__name'),
-            chapter_name=F('chapter__name'),
-            description=F('text'),
-        )
-        instance_dict = instance_qs.values().get()
-        instance_dict['thumbnail_url'] = instance.chapter.training.picture_16_9.url
-    elif isinstance(instance, Revision) and instance.is_published and instance.post.is_published:
-        instance_is_latest_revision = (
-            Revision.objects.filter(is_published=True, post=instance.post).latest('date_created')
-            == instance
-        )
-        if instance_is_latest_revision:
-            instance_qs = sender.objects.filter(id=instance.id).annotate(
-                project=Case(
-                    When(post__film__isnull=False, then=F('post__film__title')),
-                    default=Value(''),
-                    output_field=CharField(),
-                ),
-                name=F('title'),
+
+        if isinstance(instance, Film):
+            instance_dict['thumbnail_url'] = (
+                instance.picture_16_9.url if instance.picture_16_9 else instance.picture_header.url
             )
-            instance_dict = instance_qs.values().get()
+        elif isinstance(instance, Asset):
+            instance_dict['thumbnail_url'] = (
+                instance.static_asset.preview.url if instance.static_asset.preview else ''
+            )
+        elif isinstance(instance, Training):
+            instance_dict['thumbnail_url'] = instance.picture_16_9.url
+        elif isinstance(instance, Section):
+            instance_dict['thumbnail_url'] = instance.chapter.training.picture_16_9.url
+        elif isinstance(instance, Revision):
             instance_dict['thumbnail_url'] = instance.picture_16_9.url
 
-    if instance_dict:
         instance_dict['model'] = sender._meta.model_name
         instance_dict['search_id'] = f'{sender._meta.model_name}_{instance.id}'
 
