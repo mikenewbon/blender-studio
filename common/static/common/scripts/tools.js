56e2809af35d7f3ae108a7f0250e3c9a1fd1133d
Mike Newbon <michael.newbon@gmail.com>
2020-09-15 10:36:03 +0200

    Fix timestamp conversion on blogs

--- common/static/common/scripts/tools.js
+++ common/static/common/scripts/tools.js
@@ -1,14 +1,11 @@
-function EpochToDate(epoch) {
+function epochToDate(epoch) {
   if (epoch < 10000000000)
-      epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
+    epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
   var epoch = epoch + (new Date().getTimezoneOffset() * -1); //for timeZone
   return new Date(epoch);
 }
 
 function timeDifference(datetime) {
-
-  //Timestamps are provided in the search as EPOCH timestamps - so need to be converted.
-  datetime = EpochToDate(datetime);
 
   let now = new Date();
 
@@ -21,26 +18,57 @@ function timeDifference(datetime) {
   let elapsed = now - datetime;
 
   if (elapsed < msPerMinute) {
-       return Math.round(elapsed/1000) + ' seconds ago';
+    let value = Math.round(elapsed / 1000);
+    if (value == 1) {
+      return value + ' second ago'
+    } else {
+      return value + ' seconds ago';
+    }
   }
 
   else if (elapsed < msPerHour) {
-       return Math.round(elapsed/msPerMinute) + ' minutes ago';
+    let value = Math.round(elapsed / msPerMinute);
+    if (value == 1) {
+      return value + ' minute ago'
+    } else {
+      return value + ' minutes ago';
+    }
   }
 
-  else if (elapsed < msPerDay ) {
-       return Math.round(elapsed/msPerHour ) + ' hours ago';
+  else if (elapsed < msPerDay) {
+
+    let value = Math.round(elapsed / msPerHour);
+    if (value == 1) {
+      return value + ' hour ago'
+    } else {
+      return value + ' hours ago';
+    }
   }
 
   else if (elapsed < msPerMonth) {
-      return  Math.round(elapsed/msPerDay) + ' days ago';
+    let value = Math.round(elapsed / msPerDay);
+    if (value == 1) {
+      return value + ' day ago'
+    } else {
+      return value + ' days ago';
+    }
   }
 
   else if (elapsed < msPerYear) {
-      return  Math.round(elapsed/msPerMonth) + ' months ago';
+    let value = Math.round(elapsed / msPerMonth);
+    if (value == 1) {
+      return value + ' month ago'
+    } else {
+      return value + ' months ago';
+    }
   }
 
   else {
-      return Math.round(elapsed/msPerYear ) + ' years ago';
+    let value = Math.round(elapsed / msPerYear);
+    if (value == 1) {
+      return value + ' year ago'
+    } else {
+      return value + ' years ago';
+    }
   }
 }
