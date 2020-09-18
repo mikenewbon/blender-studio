7466a288099861dea180d4e73fee051db0abfa1a
railla <dev.local@gmail.com>
2020-09-18 14:24:09 +0200

    Remove sections and assets from the training index
    Removes sections and assets from `training`, `training_date_desc`, `training_date_asc` indices (doesn't change `studio` indices).
    
    Fixes https://developer.blender.org/T80886
    
    Reviewed By: mikenewbon
    
    Differential Revision: https://developer.blender.org/D8942

--- search/serializers/training_search.py
+++ search/serializers/training_search.py
@@ -1,36 +1,18 @@
-from django.db.models.expressions import F, Value
-from django.db.models.fields import CharField
 from taggit.models import Tag
 
-from films.models import Asset, AssetCategory
 from search.serializers.base import BaseSearchSerializer
-from training.models import Training, Section, TrainingStatus
+from training.models import Training, TrainingStatus
 
 
 class TrainingSearchSerializer(BaseSearchSerializer):
     """Prepare database objects to be indexed in the training search index."""
 
-    models_to_index = [Training, Section, Asset]
+    models_to_index = [Training]
     filter_params = {
         Training: {'status': TrainingStatus.published},
-        Section: {'chapter__training__status': TrainingStatus.published},
-        Asset: {
-            'is_published': True,
-            'film__is_published': True,
-            'category': AssetCategory.production_lesson,
-        },
     }
     annotations = {
         Training: {},
-        Section: {
-            'project': F('chapter__training__name'),
-            'chapter_name': F('chapter__name'),
-            'description': F('text'),
-        },
-        Asset: {
-            'project': F('film__title'),
-            'type': Value(AssetCategory.production_lesson, output_field=CharField()),
-        },
     }
     additional_fields = {
         Training: {
@@ -41,11 +23,4 @@ class TrainingSearchSerializer(BaseSearchSerializer):
             ],
             'favorite_url': lambda instance: instance.favorite_url,
         },
-        Section: {
-            'tags': lambda instance: [tag.name for tag in instance.tags.all()],
-            'secondary_tags': (
-                lambda instance: [tag.name for tag in instance.chapter.training.tags.all()]
-            ),
-        },
-        Asset: {'tags': lambda instance: [tag.name for tag in instance.tags.all()]},
     }
