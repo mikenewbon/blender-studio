b0129842613e3ff1b215675acf8fd52e28f8eab8
Francesco Siddi <francesco.siddi@gmail.com>
2020-10-20 23:58:28 +0200

    Films: Remove some admin filters
    These filters are not needed and slow down requests.

--- films/admin/films.py
+++ films/admin/films.py
@@ -16,17 +16,11 @@ class AssetAdmin(admin.ModelAdmin):
         'is_published',
         'is_featured',
         'static_asset__source_type',
-        'static_asset__user',
-        'static_asset__author',
     ]
     search_fields = [
         'name',
         'film__title',
         'collection__name',
-        'static_asset__user__first_name',
-        'static_asset__user__last_name',
-        'static_asset__author__first_name',
-        'static_asset__author__last_name',
     ]
     autocomplete_fields = ['static_asset', 'collection']
 
