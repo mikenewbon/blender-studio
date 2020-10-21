1150d782605f4a2f7c6a33db481d21a019a28d9a
Francesco Siddi <francesco.siddi@gmail.com>
2020-10-21 02:50:47 +0200

    Import: Speed up

--- cloud_import/management/commands/reconcile_users.py
+++ cloud_import/management/commands/reconcile_users.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+import pymongo
 
 from cloud_import.management import mongo
 from cloud_import.management.mixins import ImportCommand
@@ -7,13 +7,33 @@ from cloud_import.management.mixins import ImportCommand
 class Command(ImportCommand):
     help = 'Reconcile users'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-u', '--user', dest='users', action='append', help="Provide usernames")
+        parser.add_argument(
+            '--all', action='store_true', help='Reconcile all users',
+        )
+
+    def reconcile_user_with_view_progress(self, user_doc):
+        # self.stdout.write(self.style.NOTICE(f"Importing user {user_doc['username']}"))
+        # Skip Flamenco Manager users
+        if user_doc['username'].startswith('SRV-'):
+            return
+        self.console_log(f"-- Processing user {user_doc['username']}")
+        user = self.get_or_create_user(user_doc['_id'])
+        self.console_log(f"-- Processing view progress for user {user_doc['username']}")
+        self.reconcile_user_view_progress(user, user_doc)
+
     def handle(self, *args, **options):
-        for user_doc in mongo.users_collection.find({'_deleted': {'$ne': True}}):
-            # self.stdout.write(self.style.NOTICE(f"Importing user {user_doc['username']}"))
-            # Skip Flamenco Manager users
-            if user_doc['username'].startswith('SRV-'):
-                continue
-            self.console_log(f"-- Processing user {user_doc['username']}")
-            user = self.get_or_create_user(user_doc['_id'])
-            self.console_log(f"-- Processing view progress for user {user_doc['username']}")
-            self.reconcile_user_view_progress(user, user_doc)
+        if options['all']:
+            mongo.users_collection.create_index([('_updated', pymongo.DESCENDING)])
+            for user_doc in mongo.users_collection.find({'_deleted': {'$ne': True}}).sort(
+                '_updated', pymongo.DESCENDING
+            ):
+                self.reconcile_user_with_view_progress(user_doc)
+            return
+
+        for username in options['users']:
+            user_doc = mongo.users_collection.find_one(
+                {'username': username, '_deleted': {'$ne': True}}
+            )
+            self.reconcile_user_with_view_progress(user_doc)
