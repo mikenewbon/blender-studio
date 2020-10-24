00e91025dfda9299a7c101a775ca957f56813725
Anna Sirota <dev.local@gmail.com>
2020-10-24 12:27:29 +0200

    Add OAuthUserInfo into admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from profiles.models import Profile
+from blender_id_oauth_client.models import OAuthUserInfo
 
 
 @admin.register(Profile)
@@ -10,3 +11,11 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ['full_name', 'user__username', 'user__email']
     list_display = ['__str__', 'user', 'full_name', 'image_url']
     list_filter = ['is_subscribed_to_newsletter']
+
+
+@admin.register(OAuthUserInfo)
+class OAuthUserInfoAdmin(admin.ModelAdmin):
+    """Configure OAuthUserInfo admin, because blender_id_oauth_client doesn't."""
+
+    search_fields = ['user__email', 'user__username']
+    list_display = ['user', 'oauth_user_id']
