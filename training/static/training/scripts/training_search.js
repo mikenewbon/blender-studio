a4c3a436b4f5911520507f7b98379879aec8d675
Mike Newbon <michael.newbon@gmail.com>
2020-10-26 11:18:00 +0100

    Fix spacing on training search difficulty

--- training/static/training/scripts/training_search.js
+++ training/static/training/scripts/training_search.js
@@ -134,9 +134,7 @@ const renderHits = (renderOptions, isFirstRender) => {
                 <div class="card-subtitle-group">
                   <p class="card-subtitle">${ titleCase(item.type) }</p>
                   <p class="card-subtitle">
-                  ${ !!(item.difficulty) ? `
-                    <i class="material-icons icon-inline small">school</i>
-                    ${ titleCase(item.difficulty) }` : ''}
+                  ${ !!(item.difficulty) ? `<i class="material-icons icon-inline small">school</i>&nbsp;${ titleCase(item.difficulty) }` : ''}
                   </p>
                 </div>
                 <h3 class="card-title">${instantsearch.highlight({ attribute: 'name', hit: item })}</h3>
