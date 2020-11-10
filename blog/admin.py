baa78c6285c29cb7fadc7480b37e859f3d5e2f66
Anna Sirota <dev.local@gmail.com>
2020-11-10 10:42:26 +0100

    Allow setting date_published in blog post admin

--- blog/admin.py
+++ blog/admin.py
@@ -21,13 +21,16 @@ class PostAdmin(ViewOnSiteMixin, admin.ModelAdmin):
     ]
 
     def formfield_for_dbfield(self, db_field, **kwargs):
-        # Override display of excerpt filed to make it appear smaller than
-        # the content text area.
+        """Override display of "excerpt" field.
+
+        Make it appear smaller than the content text area.
+        """
         if db_field.name == 'excerpt':
             kwargs['widget'] = Textarea(attrs={'rows': 2, 'cols': 40})
         return super().formfield_for_dbfield(db_field, **kwargs)
 
     fields = (
+        'date_published',
         'title',
         'slug',
         'category',
