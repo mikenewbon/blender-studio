c6ceb0dbbea1197fd511f387e0f79d09413fa209
Anna Sirota <anna@blender.org>
2020-12-03 17:16:22 +0100

    "Deploy" script

---
+++ restart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+source /var/www/venv/bin/activate
+git pull
+./manage.py migrate --plan
+
+read -p "Continue? [y|N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Applying migrations"
+    ./manage.py migrate --plan
+    echo "Collecting static"
+    ./manage.py collectstatic --no-input
+    deactivate
+
+    echo "Restarting services"
+    systemctl restart studio.service
+    systemctl restart studio-background.service
+
+    systemctl status --no-pager studio.service
+    systemctl status --no-pager studio-background.service
+fi
