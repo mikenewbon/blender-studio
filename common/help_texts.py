071a1afaf345ff1ffcdd4bf716c83205df599371
Anna Sirota <anna@blender.org>
2020-12-11 14:23:10 +0100

    Update help text with a note about attachments

--- common/help_texts.py
+++ common/help_texts.py
@@ -1,1 +1,19 @@
-markdown = 'Format the content in <a href="https://commonmark.org/help/">Markdown</a>.'
+# noqa: D100
+markdown = '''
+<p>Format the content in <a href="https://commonmark.org/help/">Markdown</a>.</p>
+<div>
+    <p>
+        To make images float left or right of the text, use the following:
+    </p>
+    <p>
+        <code>
+        {attachment AttachmentID class='float-left'}
+        </code>
+    </p>
+    <p>
+        <code>
+        {attachment AttachmentID class='float-right'}
+        </code>
+    </p>
+</div>
+'''
