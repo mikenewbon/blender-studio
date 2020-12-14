4b91e3b56ba96f1dd7076c9da5a7d5827e8c0e83
Anna Sirota <anna@blender.org>
2020-12-14 13:53:53 +0100

    Profile settings pages [T83661]

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,11 +3,23 @@ from django.urls import path, include
 from profiles.views.activity import Activity, Notifications
 from profiles.views.api import NotificationMarkReadView, NotificationsMarkReadView
 from profiles.views.webhooks import user_modified_webhook
+import profiles.views.settings as settings
 
 urlpatterns = [
     path('webhooks/user-modified', user_modified_webhook, name='webhook-user-modified'),
     path('notifications', Notifications.as_view(), name='profile-notifications'),
     path('activity', Activity.as_view(), name='profile-activity'),
+    path(
+        'settings/',
+        include(
+            [
+                path('profile', settings.ProfileView.as_view(), name='profile-settings'),
+                path('billing', settings.BillingView.as_view(), name='profile-settings-billing'),
+                path('emails', settings.EmailsView.as_view(), name='profile-settings-emails'),
+                # path('roles', settings.RolesView.as_view(), name='profile-settings-roles'),
+            ]
+        ),
+    ),
     path(
         'api/notifications/',
         include(
