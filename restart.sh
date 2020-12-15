4025b6c47e5ba194a762e92610fd49cbfacf9a0e
Anna Sirota <anna@blender.org>
2020-12-15 17:11:51 +0100

    Include "poetry install" into the restart script

--- restart.sh
+++ restart.sh
@@ -3,14 +3,14 @@ set -e
 
 source /var/www/venv/bin/activate
 git pull
+echo "Installing dependencies"
+poetry install
 ./manage.py migrate --plan
 
 read -p "Continue? [y|N]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Installing dependencies"
-    poetry install
     echo "Applying migrations"
     ./manage.py migrate
     echo "Collecting static"
