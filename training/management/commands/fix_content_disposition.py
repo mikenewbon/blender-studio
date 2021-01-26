22952fe24ba08e7415b41da34439da0d2ffc6065
Anna Sirota <anna@blender.org>
2021-01-26 16:17:00 +0100

    Fix content headers on training videos

---
+++ training/management/commands/fix_content_disposition.py
@@ -0,0 +1,105 @@
+"""One-time helper for fixing training videos metadata on S3."""
+import logging
+import mimetypes
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+import boto3
+import botocore
+from training.models import Training
+
+logger = logging.getLogger(__name__)
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+)
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+)
+BUCKET_NAME = settings.AWS_STORAGE_BUCKET_NAME
+
+
+def _get_s3_object(key):
+    # check if this key already exists in S3
+    try:
+        return s3.Object(BUCKET_NAME, key)
+    except botocore.exceptions.ClientError as e:
+        print(e.response)
+        if e.response['Error']['Code'] != "404":
+            print(e.response)
+            raise
+
+
+class Command(BaseCommand):
+    """See below."""
+
+    help = 'Fix Content headers on existing trainings to make them downloadable.'
+
+    def _handle_video_source(self, source, content_type):
+        if not source:
+            # logger.error('No source file: %s, nothing to do', source)
+            return
+        _path = source.name
+        original_mimetype, _ = mimetypes.guess_type(_path)
+        original_name = _path.split('/')[-1]
+        assert (
+            content_type == original_mimetype
+        ), f'Wrong stored content type: {content_type} != {original_mimetype} of {_path}'
+        _file = _get_s3_object(_path)
+        if not _file:
+            logger.warning('Missing file %s', _path)
+            return
+        need_fix = False
+        if content_type != _file.content_type:
+            self.wrong_mimetype += 1
+            need_fix = True
+        if (
+            not _file.content_disposition
+            or content_type.split('/')[-1] not in _file.content_disposition
+        ):
+            self.wrong_disposition += 1
+            need_fix = True
+        content_disposition = f'attachment; filename="{original_name}"'
+        if need_fix:
+            logger.warning(
+                f'Updating: ContentDisposition "{_file.content_disposition}"'
+                f' -> "{content_disposition}",'
+                f' ContentType "{_file.content_type}" -> "{content_type}"'
+            )
+            return  # FIXME here
+            _file.copy_from(
+                CopySource={
+                    'Bucket': BUCKET_NAME,
+                    'Key': _file.key,
+                },
+                # This is the only way to update metadata, apparently
+                MetadataDirective='REPLACE',
+                ContentType=content_type,
+                ContentDisposition=content_disposition,
+            )
+
+    def _handle_video(self, asset):
+        video = asset.video
+        variations = video.variations.all()
+        for var in variations:
+            self._handle_video_source(var.source, var.content_type)
+        return
+
+    def handle(self, *args, **options):
+        """Do what is described in help."""
+        self.wrong_mimetype, self.wrong_disposition = 0, 0
+        for v in Training.objects.all():
+            for c in v.chapters.all():
+                for s in c.sections.all():
+                    asset = s.static_asset
+                    if not getattr(asset, 'video', None):
+                        continue
+                    self._handle_video(asset)
+        logger.info(
+            f'Wrong MIME-type: {self.wrong_mimetype}, '
+            f'wrong content disposition: {self.wrong_disposition}'
+        )
