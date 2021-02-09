9c76137f33e045050f47d2ac198cab89f2dc19a5
Anna Sirota <anna@blender.org>
2021-02-09 19:14:32 +0100

    Make poetry work again

--- restart.sh
+++ restart.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Just look at them go breaking teh internets:
-# https://github.com/pyca/cryptography/issues/5771#issuecomment-775016788
-export CRYPTOGRAPHY_DONT_BUILD_RUST=1
-
 set -e
 
 source /var/www/venv/bin/activate
 git pull
 echo "Installing dependencies"
-poetry install
+# Just look at them go breaking teh internets:
+# https://github.com/pyca/cryptography/issues/5771#issuecomment-775016788
+CRYPTOGRAPHY_DONT_BUILD_RUST=1 poetry install
 ./manage.py migrate --plan
 
 read -p "Continue? [y|N]" -n 1 -r
