f32b606e5f7226550ba41cadb7a17c135e8fb880
Mike Newbon <michael.newbon@gmail.com>
2021-03-24 09:34:24 +0100

    Clean up youtube modal and allow short urls

--- common/static/common/scripts/youtube_modals.js
+++ common/static/common/scripts/youtube_modals.js
@@ -1,35 +1,37 @@
 function getYouTubeID(url) {
-  let video_url = url;
-  let video_id = video_url.split('v=')[1];
-  let ampersandPosition = video_id.indexOf('&');
-  if (ampersandPosition != -1) {
-    video_id = video_id.substring(0, ampersandPosition);
+  const videoUrl = url;
+  let videoId;
+  if (videoUrl.includes('v=')) {
+    videoId = videoUrl.split('v=')[1];
+    const ampersandPosition = videoId.indexOf('&');
+    if (ampersandPosition !== -1) {
+      videoId = videoId.substring(0, ampersandPosition);
+    }
+  } else {
+    videoId = videoUrl.split('.be/')[1];
   }
-  return video_id;
+  return videoId;
 }
 
 function insertYouTubeEmbed(url) {
-  let modal = document.querySelector('.js-youtube-embed');
-  let id = getYouTubeID(url);
-  let iframeMarkup = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + id +
-    '" allowfullscreen></iframe>';
+  const modal = document.querySelector('.js-youtube-embed');
+  const id = getYouTubeID(url);
+  const iframeMarkup = `<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${id}" allowfullscreen></iframe>`;
   modal.innerHTML = iframeMarkup;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('.video-modal-link').forEach(item =>{
-    item.addEventListener('click', () =>{
-      url = item.dataset.video;
-      id = getYouTubeID(url);
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.video-modal-link').forEach((item) => {
+    item.addEventListener('click', () => {
+      const url = item.dataset.video;
       insertYouTubeEmbed(url);
-    })
+    });
   });
 
-
-  //This must be written in jQuery as the modal events can only be accessed this way.
+  // This must be written in jQuery as the modal events can only be accessed this way.
   // TODO(Mike): When Bootstrap 5 is released, change to vanilla js.
-  $('#videoModal').on('hidden.bs.modal', (e) =>{
+  $('#videoModal').on('hidden.bs.modal', (e) => {
     // When you close the modal, it's content should be removed to prevent the modal being un-hidden with the incorrect content.
-    e.target.querySelector('.js-youtube-embed').innerHTML = "";
+    e.target.querySelector('.js-youtube-embed').innerHTML = '';
   });
 });
