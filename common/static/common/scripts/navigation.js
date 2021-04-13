571f5b66ffb950c64bec9c9de608da985276500a
Mike Newbon <michael.newbon@gmail.com>
2021-04-13 17:31:49 +0200

    Fix global namespace pollution

--- common/static/common/scripts/navigation.js
+++ common/static/common/scripts/navigation.js
@@ -39,34 +39,40 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Allow side-scroll drag on desktop
-const slider = document.querySelector('.side-scroll');
-let isDown = false;
-let startX;
-let scrollLeft;
+function sideScrollNavigation() {
+  const slider = document.querySelector('.side-scroll');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-if (slider) {
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = x - startX;
-    slider.scrollLeft = scrollLeft - walk;
-  });
+  if (slider) {
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = x - startX;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  sideScrollNavigation();
+});
 
 // Progress Bar
 function animateProgress() {
