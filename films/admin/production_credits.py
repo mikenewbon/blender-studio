4ddd18ca2d7f8869e579af2cfce1667d2052e40f
Francesco Siddi <francesco.siddi@gmail.com>
2021-10-05 20:49:09 +0200

    Admin: Improve ProductionCredits listing

--- films/admin/production_credits.py
+++ films/admin/production_credits.py
@@ -7,7 +7,13 @@ from films.models import FilmProductionCredit
 class FilmProductionCreditAdmin(admin.ModelAdmin):
     autocomplete_fields = ['film', 'user']
     readonly_fields = ['date_created', 'date_updated']
-    list_display = ('film', 'display_name', 'is_public')
-    list_filter = [
+    list_display = (
         'film',
-    ]
+        'full_name',
+        'display_name',
+        'is_public',
+    )
+    list_filter = ['film', 'is_public']
+
+    def full_name(self, obj) -> str:
+        return f"{obj.user.full_name or '--'}"
