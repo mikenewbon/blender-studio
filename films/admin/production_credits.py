570bb9cb86e05c1a55aac4d410516f03b41c09a9
Anna Sirota <anna@blender.org>
2021-10-06 18:15:19 +0200

    Generate mailing list CSV from selected production credits

--- films/admin/production_credits.py
+++ films/admin/production_credits.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from films.models import FilmProductionCredit
 
@@ -14,6 +17,26 @@ class FilmProductionCreditAdmin(admin.ModelAdmin):
         'is_public',
     )
     list_filter = ['film', 'is_public']
+    actions = ['generate_mailing_list_from_selected']
 
-    def full_name(self, obj) -> str:
+    def generate_mailing_list_from_selected(self, request, queryset):
+        """Custom action for generating a CSV of emails for a mailing list."""
+        filename = 'production_credits_maillist.csv'
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={filename}'
+
+        writer = csv.writer(response)
+        for credit in queryset:
+            values = []
+            if credit.user.full_name:
+                values.append(credit.user.full_name)
+            values.append(credit.user.email)
+            writer.writerow(values)
+        return response
+
+    generate_mailing_list_from_selected.short_description = (
+        "Generate a mailing list from selected (CSV)"
+    )
+
+    def full_name(self, obj) -> str:  # noqa: D102
         return f"{obj.user.full_name or '--'}"
