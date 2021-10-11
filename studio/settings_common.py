a3a9a4b83b199d17296aeb008329bf77c30beda3
Anna Sirota <anna@blender.org>
2021-10-11 15:50:11 +0200

    Set Cache-Control header on all newly uploaded S3 files

--- studio/settings_common.py
+++ studio/settings_common.py
@@ -359,6 +359,14 @@ AWS_S3_CUSTOM_DOMAIN = 'ddz4ak4pa3d19.cloudfront.net'
 # we take care of moving the file to AWS_STORAGE_BUCKET_NAME through a
 # background task.
 AWS_UPLOADS_BUCKET_NAME = 'blender-studio-uploads'
+AWS_HEADERS = {
+    # Set max-age to 10 days
+    'Cache-Control': str('public, max-age=864000'),
+}
+# In order to set the same headers for already existing S3 keys,
+# --metadata-directive must be used, e.g.:
+#       aws s3 cp s3://blender-studio/ s3://blender-studio/ --exclude "*" --include "*.jpg" \
+#        --recursive --metadata-directive REPLACE --cache-control public,max-age=864000
 
 THUMBNAIL_STORAGE = PUBLIC_FILE_STORAGE
 THUMBNAIL_CROP_MODE = 'center'
