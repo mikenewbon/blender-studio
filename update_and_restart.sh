d9245623f44532eb921c2d78b959bccca8938b5d
Anna Sirota <anna@blender.org>
2021-10-14 16:11:18 +0200

    Try to work around more incorrect permissions in the deploy script

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -2,26 +2,30 @@
 
 set -e
 
-PYTHON_BIN=/var/www/venv/bin/python
-POETRY_BIN=/var/www/venv/bin/poetry
+VENV_PATH=/var/www/venv
+PYTHON_BIN=$VENV_PATH/bin/python
+POETRY_BIN=$VENV_PATH/bin/poetry
 DEPLOY_USER=www-data
 
 git pull
 chown $DEPLOY_USER:$DEPLOY_USER -R .
+chown $DEPLOY_USER:$DEPLOY_USER -R $VENV_PATH
 echo "Installing dependencies"
 # Just look at them go breaking teh internets:
 # https://github.com/pyca/cryptography/issues/5771#issuecomment-775016788
-CRYPTOGRAPHY_DONT_BUILD_RUST=1 sudo -u $DEPLOY_USER $POETRY_BIN install
-sudo -u $DEPLOY_USER $PYTHON_BIN manage.py migrate --plan
+sudo -Hu $DEPLOY_USER $POETRY_BIN config virtualenvs.create false
+sudo -Hu $DEPLOY_USER $POETRY_BIN config virtualenvs.in-project true
+CRYPTOGRAPHY_DONT_BUILD_RUST=1 sudo -Hu $DEPLOY_USER $POETRY_BIN install
+sudo -Hu $DEPLOY_USER $PYTHON_BIN manage.py migrate --plan
 
 read -p "Continue? [y|N]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "Applying migrations"
-    sudo -u $DEPLOY_USER $PYTHON_BIN manage.py migrate
+    sudo -Hu $DEPLOY_USER $PYTHON_BIN manage.py migrate
     echo "Collecting static"
-    sudo -u $DEPLOY_USER $PYTHON_BIN manage.py collectstatic --no-input
+    sudo -Hu $DEPLOY_USER $PYTHON_BIN manage.py collectstatic --no-input
 
     echo "Restarting services"
     systemctl restart studio.service
