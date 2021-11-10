4675b1313da70377852360f27fcc9aaf348b66a8
Anna Sirota <anna@blender.org>
2021-11-10 14:55:48 +0100

    T92715 Prepare a template for the latest assets page

---
+++ films/tests/test_latest_assets.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from common.tests.factories.films import FilmFactory
+from common.tests.factories.users import UserFactory
+
+
+class TestLatestAssets(TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        self.film = FilmFactory()
+        self.url = reverse('film-latest-assets', kwargs={'film_slug': self.film.slug})
+
+    def test_view_not_logged_in(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_logged_in(self):
+        user = UserFactory()
+        self.client.force_login(user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
