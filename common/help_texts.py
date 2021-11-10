e637b9084fc85735c8f59d4218e1da178828a1b3
Anna Sirota <anna@blender.org>
2021-11-10 11:01:47 +0100

    Fix markdown fields migration

--- common/help_texts.py
+++ common/help_texts.py
@@ -1,6 +1,4 @@
 # noqa: D100
-from django.templatetags.static import static
-
 import common.markdown
 
 markdown = '''
@@ -22,9 +20,7 @@ markdown = '''
 </div>
 '''
 allowed_tags = ', '.join(f'<{tag}>' for tag in common.markdown.ALLOWED_TAGS)
-q_img = static('admin/img/icon-unknown.svg')
 markdown_with_html = markdown + (
     f'<p title="{allowed_tags}">Some HTML tags are allowed&nbsp;'
-    f'<img alt="Allowed HTML tags" src="{q_img}" class="help help-tooltip">'
-    '</p>'
+    '<span class="help help-tooltip">ℹ</span>'
 )
