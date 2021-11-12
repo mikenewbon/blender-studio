bd10d63f0c7e230142d4889927c40b96c48a28b3
Anna Sirota <anna@blender.org>
2021-11-12 18:05:40 +0100

    Minor change to ajax tool: pass json on 400 Bad Request

--- common/static/common/scripts/ajax.js
+++ common/static/common/scripts/ajax.js
@@ -32,6 +32,9 @@ window.ajax = (function ajax() {
         if (response.status >= 200 && response.status < 300) {
           return Promise.resolve(response);
         }
+        if (response.status === 400) {
+          return Promise.reject(response.json());
+        }
         return Promise.reject(new Error(response.statusText));
       })
       .then(response => response.json());
